bf772cb07e79f7737c4e80fa90a744094ac46c54
Igor Kuriy <kuriyio@gmail.com>
2015-10-26 00:57:21 +0200

    device menu

--- eco/eco/eco.asm
+++ eco/eco/eco.asm
@@ -2,6 +2,7 @@
 
  .def temp=r17		   //define r16 as temp
  .def byte=r16		   //define r17 as byte (will be used for send byte to LCD)
+ .def menu_loc=r18
 
 //==========DATA SEGMENT=================================================
 // In this segment we can to reserve some memory
@@ -9,39 +10,49 @@
 .dseg
 .org 0x0060						//starting from 0x0060 because 
 								//before that adress we have reserved memory for registers
-hello:				.byte 16	//hello world string
+CO_msg:				.byte 16	//hello world string
 CH4_msg:			.byte 16	//CH4 level message string
 temperature_msg:	.byte 16	//Temperature message string
 time_msg:			.byte 16	//Time message string
 coords_msg:			.byte 16	//Coordinates message string
 sound_msg:			.byte 16	//Sound option message string
+test:				.byte 96	//reserve bytes for 2nd string messages
+menu_location:		.byte 1		//current menu location
 
 //==========CODE SEGMENT=================================================
 .cseg
 .include "int_table.inc"	//interrupt vectors
+.include "menu.inc"			//macro for menu uses
 .include "LCD.inc"			//macro and procedures for HD44780 like displays
 .include "keyboard.inc"		//macro and procedures for 3x4 keyboard
+
 reset:
 .include "core_init.inc"	//clear sram, registers and stack init
 //-----------Program start-------------------------
-load_strings_to_SRAM		//save strings from eeprom to ram
 
 keyboard_init				//initialization of ports for keyboard
 LCD_init					//LCD initialization
+menu_init
 
 //-----------MAIN----------------------------------
 main:			
-	key_survay		//check for buttons press
+	keyboard_check		//check for buttons press
 rjmp main		
 //-------------------------------------------------
 //========EEPROM SEGMENT=================================================
 .eseg
 .org 0
-ee_hello_msg:	.db "Hello World!    "
-ee_CH4_msg:		.db "Уровень СН4     "
-ee_temp_msg:	.db "Температура     "
-ee_time_msg:	.db "Время           "
-ee_coords_msg:	.db "Координаты      "
-ee_sound_msg:	.db "Звук (5-измен.) "
+ee_hello_msg:	.db "Level CO        "
+ee_CH4_msg:		.db "Level CH4      ",0
+ee_temp_msg:	.db "Temperature    ",0
+ee_coords_msg:	.db "Coordinates    ",0
+ee_time_msg:	.db "Time           ",0
+ee_sound_msg:	.db "Sound(5-On/Off)",0
+ee_test_msg1:	.db "test str 1     ",1
+ee_test_msg2:	.db "test str 2     ",1
+ee_test_msg3:	.db "test str 3     ",1
+ee_test_msg4:	.db "test str 4     ",1
+ee_test_msg5:	.db "test str 5     ",1
+ee_test_msg6:	.db "test str 6      "
 //=======================================================================
 //                   Sorry for bad english
